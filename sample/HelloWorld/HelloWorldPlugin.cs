c3e0ae80eaba75a843a6d48f44cd90e27710775e
evgeniy <evgenij.kriajew@yandex.ru>
2026-09-09 04:00:24 +0500

    Update sample mod

--- sample/HelloWorld/HelloWorldPlugin.cs
+++ sample/HelloWorld/HelloWorldPlugin.cs
@@ -4,10 +4,11 @@ namespace F4Forge.Sample.HelloWorld;
 
 public sealed class HelloWorldPlugin : F4ForgePlugin
 {
-    public override string Id => "sample.hello_world";
+    public override string Id => "mod.hello_world";
 
-    public override void OnLoad()
+    public override void OnLoad(F4ForgePluginContext context)
     {
+        context.CancellationToken.ThrowIfCancellationRequested();
         Logger.Info("Hello world from F4Forge!");
     }
 }
