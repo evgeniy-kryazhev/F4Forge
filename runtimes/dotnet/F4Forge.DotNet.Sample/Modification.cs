ae28fb71d9a036b4a1f4b3f7e09bf367c9cc930a
evgeniy <evgenij.kriajew@yandex.ru>
2026-09-11 22:46:08 +0500

    Add sample

---
+++ runtimes/dotnet/F4Forge.DotNet.Sample/Modification.cs
@@ -0,0 +1,17 @@
+﻿using F4Forge.DotNet.Sdk;
+
+namespace F4Forge.DotNet.Sample;
+
+public class Modification : F4ForgePlugin
+{
+    public override void OnLoad(F4ForgePluginContext context)
+    {
+        context.Events.GameDataReady += OnGameDataReady;
+        context.Events.GameLoaded += OnGameLoaded;
+        context.Events.NewGame += OnNewGame;
+    }
+
+    private static void OnGameDataReady() { }
+    private static void OnGameLoaded() { }
+    private static void OnNewGame() { }
+}
